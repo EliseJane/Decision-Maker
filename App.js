177279ba9f3f706af99c7b4d42454ffd4421d226
Elise Olivares <elisejpo@gmail.com>
2018-03-13 17:20:56 -0700

    question query

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import {
   View,
   Button,
   Picker,
+  Alert,
 } from 'react-native';
-import ModalDropdown from 'react-native-modal-dropdown';
 
 import SQLite from 'react-native-sqlite-storage';
 SQLite.DEBUG(true);
@@ -16,29 +16,190 @@ const database_name = "Audrey.db";
 const database_version = "1.0";
 const database_displayname = "Audrey's Favorite Things";
 const database_size = 200000;
-let db;
 
-type Props = {};
 export default class App extends Component<Props> {
   constructor() {
     super();
+    this.db = null;
     this.state = {
       selected: 0,
-      questions: [
-        "Add a new question...",
-        'What should I do with the rest of my paycheck?',
-        'What movie should I watch tonight?',
-        'Where should I go have dinner?',
-        'What color socks should I wear?'
-      ]
+      questions: ['What are you trying to decide?']
     }
+    this.loadAndQueryDB();
+  }
+
+  loadAndQueryDB = () => {
+    SQLite.echoTest().then(() => {
+      SQLite.openDatabase({name : "Audrey.db", createFromLocation : "~/db/Audrey.db"}).then((DB) => {
+        this.db = DB;
+        this.db.transaction(this.populateInitialData).then(() => {
+          this.db.transaction(this.queryQuestions).catch(this.errorCB);
+        }).catch(this.errorCB);
+      }).catch(this.errorCB);
+    }).catch(this.errorCB);
+  }
+
+  clearDB = (tx) => {
+    tx.executeSql('DROP TABLE IF EXISTS Questions;');
+    tx.executeSql('DROP TABLE IF EXISTS Options;');
+  }
+
+  populateInitialData = (tx) => {
+    this.clearDB(tx);
+
+    // in future: no clearDB, only create table if not exist statements
+
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Questions( '
+        + 'id INTEGER PRIMARY KEY NOT NULL, '
+        + 'question VARCHAR(50) UNIQUE ); ').catch((error) => {
+        this.errorCB(error)
+    });
+
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Options( '
+        + 'id INTEGER PRIMARY KEY NOT NULL, '
+        + 'option VARCHAR(50) UNIQUE, '
+        + 'question_id INTEGER, '
+        + 'FOREIGN KEY ( question_id ) REFERENCES Questions ( id ) ); ').catch((error) => {
+        this.errorCB(error)
+    });
+
+    tx.executeSql('INSERT INTO Questions (question) VALUES ("What should I do with the rest of my paycheck?");');
+    tx.executeSql('INSERT INTO Questions (question) VALUES ("What color socks should I wear?");');
+
+    tx.executeSql('INSERT INTO Options (option, question_id) VALUES ("Get a pedicure!", 1);');
+    tx.executeSql('INSERT INTO Options (option, question_id) VALUES ("Pay bills :-( ", 1);');
+    tx.executeSql('INSERT INTO Options (option, question_id) VALUES ("Buy school supplies", 1);');
+    tx.executeSql('INSERT INTO Options (option, question_id) VALUES ("Red", 2);');
+    tx.executeSql('INSERT INTO Options (option, question_id) VALUES ("Blue", 2);');
+    tx.executeSql('INSERT INTO Options (option, question_id) VALUES ("Purple", 2);');
+    tx.executeSql('INSERT INTO Options (option, question_id) VALUES ("Black", 2);');
+  }
+
+  // checkIfPopulated = (tx) => {
+  //   console.log("INSIDE CHECKIFPOPULATED");
+  //   tx.executeSql('SELECT COUNT(question) as q FROM Questions;').catch(() => {
+  //     console.log("INSIDE ERROR CATCH");
+  //     this.populateInitialData(tx).then((tx) => {
+  //       this.queryQuestions(tx);
+  //     }).catch(error=>console.log(error));
+  //   }).then(([tx, results]) => {
+  //     console.log("INSIDE SELECT COUNT");
+  //     if (results.rows.item(0).q < 2) {
+  //       console.log("INSIDE SELECT COUNT IF");
+  //       this.populateInitialData(tx).then((tx) => {
+  //         this.queryQuestions(tx);
+  //       }).catch(error=>console.log(error));
+  //     } else {
+  //       console.log("INSIDE SELECT COUNT ELSE");
+  //       this.queryQuestions(tx);
+  //     }
+  //   });
+  // }
+
+  queryQuestions = (tx) => {
+    tx.executeSql('SELECT question FROM Questions;').then(([tx,results]) => {
+      const len = results.rows.length;
+      let questions = this.state.questions;
+      for (let i = 0; i < len; i++) {
+        let row = results.rows.item(i);
+        questions.push(row.question);
+      }
+      this.setState({questions: questions});
+    }).catch(this.errorCB);
+  }
+
+  // queryOptions = (tx) => {
+  //   tx.executeSql('SELECT option, question_id FROM Options;').then(([tx,results]) => {
+  //     const len = results.rows.length;
+  //     for (let i = 0; i < len; i++) {
+  //         let row = results.rows.item(i);
+  //         console.log(`Option: ${row.option}, Question id: ${row.question_id}`)
+  //     }
+  //   }).catch((error) => {
+  //       console.log(error);
+  //   });
+  // }
+
+  newQuestion = () => {
+
+  }
+
+  pickRandom = () => {
+    const q = this.state.questions[this.state.selected];
+    this.db.transaction(tx => {
+      tx.executeSql(`SELECT option FROM Options WHERE Options.question_id = (SELECT id AS q_id FROM Questions WHERE question = '${q}')`)
+      .then( ([tx,result]) => {
+        const len = result.rows.length;
+        const rand = Math.floor(Math.random() * len);
+        Alert.alert(
+          result.rows.item(rand).option,
+          null,
+          [{text: "OK", onPress: () => {}}]
+        )
+      }).catch(this.errorCB);
+    }).catch(this.errorCB);
+  }
+
+  seeOptions = () => {
+
+  }
+
+  deleteAlert = () => {
+    Alert.alert(
+      'Delete Question',
+      'Are you sure you want to delete this question and all of its associated options?',
+      [
+        {text: "Yes, I'm sure", onPress: this.deleteQuestion, style: 'destructive'},
+        {text: 'Cancel', onPress: () => {}, style: 'cancel'}
+      ]
+    )
+  }
+
+  deleteQuestion = () => {
+    let questions = this.state.questions;
+    const question = questions[this.state.selected];
+
+    this.db.transaction((tx) => {
+      tx.executeSql(`DELETE FROM Questions WHERE question = '${question}';`)
+    }).then(() => {
+      questions.splice(this.state.selected, 1);
+      this.setState({questions: questions, selected: 0});
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  errorCB = (err) => {
+    console.log("error: ", err);
   }
 
   render() {
-    const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
     const items = this.state.questions.map((q, i) => {
       return <Picker.Item label={q} value={q} key={i} />
-    })
+    });
+
+    const buttons = this.state.selected === 0 || this.state.questions.length === 1 ?
+      (<Button
+        onPress={this.newQuestion}
+        title="Add new question"
+        color='red'
+      />) :
+      (<View style={styles.row}>
+        <Button
+          onPress={this.pickRandom}
+          title="Pick for me!"
+          color="red"
+        />
+        <Button
+          onPress={this.seeOptions}
+          title="See all options"
+          color="red"
+        />
+      </View>);
+
+    const disabled = this.state.selected === 0 || this.state.questions.length === 1 ?
+        true : false;
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -58,26 +219,21 @@ export default class App extends Component<Props> {
           <Text style={{color: '#bb00ff'}}>r</Text>
           <Text style={{color: '#870067'}}>!</Text>
         </Text>
-        <Text style={styles.prompt}>
-          What are you trying to decide?
-        </Text>
-        <Picker
-          style={styles.menu}
-          itemStyle={styles.menuItem}
-          selectedValue={this.state.questions[this.state.selected]}
-          onValueChange={(itemValue, itemIndex) => this.setState({selected: itemIndex})}
-        >
-          {items}
-        </Picker>
+          <Picker
+            style={styles.menu}
+            itemStyle={styles.menuItem}
+            selectedValue={this.state.questions[this.state.selected]}
+            onValueChange={(itemValue, itemIndex) => this.setState({selected: itemIndex})}
+          >
+            {items}
+          </Picker>
+        {buttons}
         <Button
-          onPress={() => console.log(this.state.selected)}
-          title="Pick for me!"
-          color="red"
-        />
-        <Button
-          onPress={() => console.log(this.state.selected)}
-          title="See all options"
-          color="red"
+          disabled={disabled}
+          onPress={this.deleteAlert}
+          title="Delete question"
+          color="yellow"
+          style={styles.delete}
         />
       </View>
     );
@@ -92,19 +248,22 @@ const styles = StyleSheet.create({
     flexDirection: 'column'
   },
   welcome: {
-    fontSize: 30,
+    fontSize: 40,
     fontWeight: 'bold',
-    textAlign: 'center',
-    margin: 10,
-  },
-  prompt: {
-    color: 'red',
-    fontSize: 20,
-    textAlign: 'center',
-    marginTop: 30
+    textAlign: 'center'
   },
   menuItem: {
     color: 'lightgrey',
     fontSize: 16,
+  },
+  menu: {
+    margin: 20
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  delete: {
+    fontSize: 10
   }
 });
